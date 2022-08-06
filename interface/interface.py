b918b9f1d006c20906d6164da517c06f4e24a2fa
Ian Mota <ianmotabr@gmail.com>
2022-08-05 23:11:07 -0300

    exe-generate-00

--- interface/interface.py
+++ interface/interface.py
@@ -4,6 +4,7 @@ from threading import *
 from backend.scrapper.scrapper import ScrapperColect, ScrapperResearch
 import base64
 from data.dataSave import *
+from os.path import realpath
 
 class Application(ScrapperResearch,ScrapperColect):
     
@@ -21,8 +22,8 @@ class Application(ScrapperResearch,ScrapperColect):
         self.root.geometry('400x300')
         self.root.resizable(False,False)
         self.root.protocol("WM_DELETE_WINDOW", self._close)
-        
-        with open("icons\Projete5D140x140png.png", "rb") as image_file:
+ 
+        with open(realpath("icons\Projete5D140x140png.png"), "rb") as image_file:
             image64 = base64.b64encode(image_file.read())
         self.logoP5D = PhotoImage(data=image64)
         self.root.iconphoto(False,self.logoP5D)
@@ -54,10 +55,9 @@ class Application(ScrapperResearch,ScrapperColect):
         self.en_senha.place(relx=0.43,rely=0.6,relwidth=en_width)
         
     def Root01Button(self):    
-        self.bt_confirmar = Button(self.root, text="Confirmar", font= ('verdana',11,'bold'),command=self._get_html)
+        self.bt_confirmar = Button(self.root, text="Confirmar", font= ('verdana',11,'bold'),command=self._th_get_html)
         self.bt_confirmar.place(relx=0.4, rely=0.85)
         
-        self.localSave_value = StringVar()
         self.bt_localSave = Button(self.root, font= ('verdana',11,'bold'), justify='center',command=self._get_folder,text="Local")
         self.bt_localSave.place(relx=0.43, rely=0.7, relwidth=0.55, height=26)
 
@@ -75,26 +75,47 @@ class Application(ScrapperResearch,ScrapperColect):
         
     def _get_html(self):
         
-        self.lb_fraseImpacto["text"] = 'Iniciando...'
-        self.lb_fraseImpacto["font"] = ('verdana',9)
-        
         email = str(self.en_usuario.get())
         password = str(self.en_senha.get())
 
         
-        
-        self.browser = ScrapperResearch(email,password)
-        self.browser.openResearch()
-        self.browser.addLogin()
-        self.browser.addSenha()
-        self.browser.addCompany()
-        self.browser.selectProject()
-        self.browser.goToIssues()
-        self.browser.openAll()    
-        self.browser.htmlGenerator()
-        self.lb_fraseImpacto["text"] = 'Coletando dados'
+        self.lb_fraseImpacto["text"] = 'Abrindo servidor'
         self.lb_fraseImpacto["font"] = ('verdana',9)
         self.lb_fraseImpacto["fg"] = '#008000'
+        
+        self.browser = ScrapperResearch(email,password)
+        try:
+            self.browser.openResearch()
+            self.browser.addLogin()
+            self.browser.addSenha()
+            self.browser.addCompany()
+            self.browser.selectProject()
+            self.browser.goToIssues()
+            self.browser.openAll()
+        except:   
+            self.lb_fraseImpacto["text"] = 'Falha na abertura'
+            self.lb_fraseImpacto["font"] = ('verdana',9)
+        else:
+            self.lb_fraseImpacto["text"] = 'Coletando dados, aguarde. Esta \
+                \n  operação costuma demorar \n 10 minutos'
+            self.lb_fraseImpacto["font"] = ('verdana',9)
+            self.lb_fraseImpacto["fg"] = '#008000'
+        
+        try:
+            self.browser.htmlGenerator()
+            
+        except:
+            self.lb_fraseImpacto["text"] = 'Não foi possível coletar\
+                \n os dados. ERRO!'
+            self.lb_fraseImpacto["font"] = ('verdana',9)
+            
+        else:
+            self.lb_fraseImpacto["text"] = 'Dados coletados, construindo\
+                \n o banco de dados bruto'
+            self.lb_fraseImpacto["font"] = ('verdana',9)
+            self.lb_fraseImpacto["fg"] = '#008000'
+        
+        
 
         self._create_dictionary()
         
@@ -119,47 +140,45 @@ class Application(ScrapperResearch,ScrapperColect):
 
             
         self.dicionario = {}
-        print(len(self.browser.htmlList))
-        # for i in self.browser.htmlList:
-        #     coleta = ScrapperColect(i)
-        #     dicBase = coleta.dictionaryGeneration()
+        for i in self.browser.htmlList:
+            coleta = ScrapperColect(i)
+            dicBase = coleta.dictionaryGeneration()
             
-        #     id.append(dicBase.get("ID"))
-        #     titulo.append(dicBase.get("Título"))    
-        #     status.append(dicBase.get("Status")) 
-        #     descricao.append(dicBase.get("Descrição")) 
-        #     empreendimento.append(dicBase.get("Empreendimento"))
-        #     etiqueta.append(dicBase.get("Etiqueta"))
-        #     responsavel.append(dicBase.get("Responsável"))
-        #     fase.append(dicBase.get("Fase"))
-        #     tipo.append(dicBase.get("Tipo"))
-        #     area.append(dicBase.get("Local"))
-        #     prazo.append(dicBase.get("Prazo"))
-        #     prioridade.append(dicBase.get("Prioridade"))
-        #     ultimaAlteracao.append(dicBase.get("Última modificação"))
-        #     primeiraAlteracao.append(dicBase.get("Primeira alteração"))
+            id.append(dicBase.get("ID"))
+            titulo.append(dicBase.get("Título"))    
+            status.append(dicBase.get("Status")) 
+            descricao.append(dicBase.get("Descrição")) 
+            empreendimento.append(dicBase.get("Empreendimento"))
+            etiqueta.append(dicBase.get("Etiqueta"))
+            responsavel.append(dicBase.get("Responsável"))
+            fase.append(dicBase.get("Fase"))
+            tipo.append(dicBase.get("Tipo"))
+            area.append(dicBase.get("Local"))
+            prazo.append(dicBase.get("Prazo"))
+            prioridade.append(dicBase.get("Prioridade"))
+            ultimaAlteracao.append(dicBase.get("Última modificação"))
+            primeiraAlteracao.append(dicBase.get("Primeira alteração"))
             
             
-        # self.dicionario["ID"] = id
-        # self.dicionario["Título"] = titulo
-        # self.dicionario["Status"] = status
-        # self.dicionario["Descrição"] = descricao
-        # self.dicionario["Empreendimento"] = empreendimento
-        # self.dicionario["Etiqueta"] = etiqueta
-        # self.dicionario["Responsável"] = responsavel
-        # self.dicionario["Fase"] = fase
-        # self.dicionario["Tipo"] = tipo
-        # self.dicionario["Local"] = area
-        # self.dicionario["Prazo"] = prazo
-        # self.dicionario["Prioridade"] = prioridade
-        # self.dicionario["Última alteração"] = ultimaAlteracao
-        # self.dicionario["Primeira alteração"] = primeiraAlteracao
-
-        # print(self.dicionario)
+        self.dicionario["ID"] = id
+        self.dicionario["Título"] = titulo
+        self.dicionario["Status"] = status
+        self.dicionario["Descrição"] = descricao
+        self.dicionario["Empreendimento"] = empreendimento
+        self.dicionario["Etiqueta"] = etiqueta
+        self.dicionario["Responsável"] = responsavel
+        self.dicionario["Fase"] = fase
+        self.dicionario["Tipo"] = tipo
+        self.dicionario["Local"] = area
+        self.dicionario["Prazo"] = prazo
+        self.dicionario["Prioridade"] = prioridade
+        self.dicionario["Última alteração"] = ultimaAlteracao
+        self.dicionario["Primeira alteração"] = primeiraAlteracao
+        
+        self._save()
         
     def _save(self):
-        localSave = str(self.localSave_value.get())
         nomeEmail = self.en_usuario.get()
         nomeArquivo = nomeEmail.split('@')
         
-        print(f"{localSave}/{nomeArquivo}.csv")
+        dbBuildCSV(self.dicionario,f'{self.en_localSalve}/{nomeArquivo[0]}.csv')
